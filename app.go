c02573ff61142f940385b1645bc63f4c9f8a1fc0
Nathan Sanchez <nathansanchezdev@outlook.com>
2025-02-17 21:12:12 -0500

    Modular main app.go file.

--- app.go
+++ app.go
@@ -2,34 +2,31 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
 )
 
+const (
+	InvestmentCalculator = "invest"
+	ProfitCalculator     = "profit"
+)
+
+func DecideExecutionType() {
+	var executionType string
+	fmt.Print("Which program do you want to run? (invest/profit): ")
+	fmt.Scan(&executionType)
+
+	executionType = strings.ToLower(strings.TrimSpace(executionType))
+
+	switch executionType {
+	case InvestmentCalculator:
+		RunInvestmentCalculator()
+	case ProfitCalculator:
+		fmt.Println("Adding soon")
+	default:
+		fmt.Println("Invalid input. Please enter 'invest' or 'profit'.")
+	}
+}
+
 func main() {
-	getData()
-}
-
-func getData() {
-	var investmentAmount float64
-	var expectedReturnRate float64
-	var years float64
-
-	readData("How much to invest?: ", &investmentAmount)
-	readData("Expected return rate?: ", &expectedReturnRate)
-	readData("How many years to invest?: ", &years)
-
-	CalculateInvestment(investmentAmount, expectedReturnRate, years)
-}
-
-func readData(printValue string, scanPointer *float64) {
-	fmt.Print(printValue)
-	fmt.Scan(scanPointer)
-}
-
-func CalculateInvestment(investmentAmount float64, expectedReturnRate float64, years float64) {
-	calc := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	realCalc := calc / math.Pow(1+2.5/100, years)
-
-	fmt.Println("Calc", calc)
-	fmt.Println("realCalc", realCalc)
+	DecideExecutionType()
 }
