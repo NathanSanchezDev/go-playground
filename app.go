300ca9a8a3d40c9100b648dd409c13a2ea9e933f
Nathan Sanchez <nathansanchezdev@outlook.com>
2025-02-24 22:28:42 -0500

    add case condition for bank

--- app.go
+++ app.go
@@ -4,17 +4,19 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/NathanSanchezDev/go-playground/bank"
 	"github.com/NathanSanchezDev/go-playground/calculator"
 )
 
 const (
 	InvestmentCalculator = "invest"
 	ProfitCalculator     = "profit"
+	Bank                 = "bank"
 )
 
 func DecideExecutionType() {
 	var executionType string
-	fmt.Print("Which program do you want to run? (invest/profit): ")
+	fmt.Print("Which program do you want to run? (invest/profit/bank): ")
 	fmt.Scan(&executionType)
 
 	executionType = strings.ToLower(strings.TrimSpace(executionType))
@@ -24,6 +26,8 @@ func DecideExecutionType() {
 		calculator.RunInvestmentCalculator()
 	case ProfitCalculator:
 		fmt.Println("Adding soon")
+	case Bank:
+		bank.Bank()
 	default:
 		fmt.Println("Invalid input. Please enter 'invest' or 'profit'.")
 	}
